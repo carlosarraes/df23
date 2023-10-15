7cff7369978ec896b97551b1971e41d83736a23d
carraes <carraeshb@gmail.com>
2023-10-14 22:58:39 -0300

    feat(nvim): add copilot

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,50 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		local copilot = require("copilot")
+
+		copilot.setup({
+			panel = {
+				enabled = true,
+				auto_refresh = false,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-4>",
+				},
+				layout = {
+					position = "bottom",
+					ratio = 0.4,
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<M-1>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-2>",
+					prev = "<M-3>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			copilot_node_command = "node",
+			server_opts_overrides = {},
+		})
+	end,
+}
