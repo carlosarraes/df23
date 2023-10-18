b78e1d5a3742723d373f2d53c595c4d044b59c29
carraes <carraeshb@gmail.com>
2023-10-18 00:03:32 -0300

    fix(nvim): fix icon

--- nvim/.config/nvim/lua/plugins/lsp/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp.lua
@@ -41,7 +41,7 @@ return {
 			end
 		end
 
-		local signs = { Error = " ", Warn = " ", Hint = "H ", Info = " " }
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
