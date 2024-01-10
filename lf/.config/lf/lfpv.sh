17add67d08da1889a1fa98975e10c6efb3387a69
carraes <carraeshb@gmail.com>
2024-01-10 00:22:11 -0300

    feat(lf): add lf support

---
+++ lf/.config/lf/lfpv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) unzip -l "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+    *.pdf) pdftotext "$1" -;;
+    *) bat --paging=never --style=numbers --theme="base16" --terminal-width $(($2-5)) -f "$1" || true;;
+esac
