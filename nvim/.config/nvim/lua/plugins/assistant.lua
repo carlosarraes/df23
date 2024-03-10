4d3a96444a82b5595e1126694d805d0c2688a6b6
carraes <carraeshb@gmail.com>
2024-03-10 11:27:17 -0300

    feat(nvim): update lock, some minor changes

--- nvim/.config/nvim/lua/plugins/assistant.lua
+++ nvim/.config/nvim/lua/plugins/assistant.lua
@@ -3,7 +3,7 @@ return {
 	event = "VeryLazy",
 	config = function()
 		local defaults = {
-			api_key_cmd = "pass show chatgpt/api_key",
+			api_key_cmd = "pass show openapi/key",
 			yank_register = "+",
 			edit_with_instructions = {
 				diff = false,
