8e0943d3da005e0fa567621646b0a1892aa9add7
carraes <carraeshb@gmail.com>
2024-06-13 15:13:27 -0300

    feat(nvim): update oil settings

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,6 +5,20 @@ return {
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
+			skip_confirm_for_simple_edits = true,
+			keymaps = {
+				["<C-s>"] = false,
+			},
+			view_options = {
+				show_hidden = true,
+				natural_order = true,
+				is_always_hidden = function(name)
+					return name == ".."
+				end,
+			},
+			preview = {
+				border = "single",
+			},
 		})
 
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>")
